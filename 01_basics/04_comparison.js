6ab152719f03314c4654dcf81e4ea82ee2b07d6f
MANISH JAISWAL <120405796+MANISHJAISWA@users.noreply.github.com>
2024-04-24 13:26:06 +0000

    compare

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,27 @@
+// console.log(2 > 2);
+// console.log(2 >= 2);
+// console.log(2 < 2);
+// console.log(2 <= 2);
+// console.log(2 == 2);
+// console.log(2 != 2);
+
+// console.log("0" < 1);  // true
+// console.log("06" > 8);  // false
+
+// console.log(null > 0);  //false
+// console.log(null >= 0); // true
+// console.log(null < 0);  //false
+// console.log(null <= 0);  //true
+// console.log(null == 0);   // false
+// console.log(null != 0);   // true
+
+// console.log(undefined > 0);  //false
+// console.log(undefined >= 0); // false
+// console.log(undefined < 0);  //false
+// console.log(undefined <= 0);  //false
+// console.log(undefined == 0);   // false
+// console.log(undefined != 0);   // true
+
+// ===
+console.log(2 == "2")  // true
+console.log(2 === "2")  //false
