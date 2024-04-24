1e848041fc9e83db15fbb18153ac87ed40694c85
MANISH JAISWAL <120405796+MANISHJAISWA@users.noreply.github.com>
2024-04-24 11:39:46 +0000

    changed

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -40,9 +40,22 @@ let value = 3
 let negValue = -value
 // console.log(negValue)
 
-console.log(2+2)
-console.log(2-2)
-console.log(2*2)
-console.log(2/2)
-console.log(2%2)
-console.log(2^2)
+// console.log(2 + 2)
+// console.log(2 - 2)
+// console.log(2 * 2)
+// console.log(2 / 2)
+// console.log(2 ** 2)
+// console.log(2 % 2)
+
+let str1 = "Hello"
+let str2 = " Manish"
+let str3 = str1 + str2
+// console.log(str3)
+
+
+// console.log(1 + "3")
+// console.log("1" + 4)
+// console.log("2" + 3 + 7)
+// console.log(2 + 3 + "7")
+// console.log((((3 + 4 )* 4 )%3))
+
