90d7832b1cab3c0cc8dd47b579a70d22c43d32a0
MANISH JAISWAL <120405796+MANISHJAISWA@users.noreply.github.com>
2024-04-24 20:42:02 +0000

    new

---
+++ 01_basics/06_nums_math.js
@@ -0,0 +1,18 @@
+const score = 500
+// console.log(score)
+
+const balance = new Number(200)
+// console.log(balance)
+
+// console.log(balance.toString())
+// console.log(typeof balance.toString())
+// console.log(balance.toString().length)
+
+// console.log(balance.toFixed(3))
+
+const otherNumber = 23.4383432
+// console.log(otherNumber.toPrecision(4))
+
+const hundreds = 100000000000000
+console.log(hundreds.toLocaleString())
+console.log(hundreds.toExponential())
