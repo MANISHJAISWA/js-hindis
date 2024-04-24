cefb760260bc1d5b6fb4c22c9c1dc5ec413f2a38
MANISH JAISWAL <120405796+MANISHJAISWA@users.noreply.github.com>
2024-04-24 12:58:55 +0000

    incr/dec operator

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -53,9 +53,17 @@ let str3 = str1 + str2
 // console.log(str3)
 
 
-// console.log(1 + "3")
-// console.log("1" + 4)
-// console.log("2" + 3 + 7)
-// console.log(2 + 3 + "7")
-// console.log((((3 + 4 )* 4 )%3))
+// console.log(1 + "3");
+// console.log("1" + 4);
+// console.log("2" + 3 + 7);
+// console.log(2 + 3 + "7");
+// console.log((((3 + 4 )* 4 )%3));
 
+// console.log(+"");
+
+let num1, num2, num3
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+gameCounter--
+console.log(gameCounter)
