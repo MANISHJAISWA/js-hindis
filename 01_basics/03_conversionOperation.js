5dd7ff80cdaaed8e7f877fb45d8598a2ec31974f
MANISH JAISWAL <120405796+MANISHJAISWA@users.noreply.github.com>
2024-04-24 11:07:09 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,36 @@
+let score = "manish"
+
+// console.log(typeof score)
+// console.log(typeof (score))
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+
+
+// string to number
+// "33"  => 33
+//"33andb"  => NaN
+//"Manish"  => NaN
+// null  => 0
+// undefined => NaN
+// true => 1
+// false => 0
+
+let isLoggedIn = ""
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// conversion in boolean
+// 1 => true  // 0 => false
+// " " => false
+// "manish" => true
+// null => false
+// undefined => false
+
+
+let someNumber = 45
+let stringNumber = String(someNumber)
+console.log(someNumber)
+console.log(typeof stringNumber)
+
