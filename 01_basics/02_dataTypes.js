27c431437042a94926d11551b6dc2ae92a806405
MANISH JAISWAL <120405796+MANISHJAISWA@users.noreply.github.com>
2024-04-24 10:37:35 +0000

    datatypes

--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -5,3 +5,24 @@
 console.log(3 + 3)
 
 console.log("Manish Jaiswal")
+
+let name = "Manish"
+let age = 13
+let isLoggedIn = false
+
+ //number =>2 power 53
+ //bigInt
+ //string = ""
+ //boolean => true/false
+ // null => standalone value
+ // undefined
+ // symbol => unique
+
+
+ //object
+
+ console.log(typeof "manish")
+console.log(typeof age)
+console.log(typeof null)
+console.log(typeof undefined)
+
