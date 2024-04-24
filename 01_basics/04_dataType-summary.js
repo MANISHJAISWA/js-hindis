ee6f6d242a299a0da351d183216d3d5dd4bf2cf3
MANISH JAISWAL <120405796+MANISHJAISWA@users.noreply.github.com>
2024-04-24 14:20:42 +0000

    datatype summary

---
+++ 01_basics/04_dataType-summary.js
@@ -0,0 +1,49 @@
+// primitive  - call by value
+
+// 7 types : String, Number, Boolean, null, undefined, symbol, BigInt
+
+const isLoggedIn = true;
+const num = 67.23
+const outsidetemp = null;
+
+let userName;
+const id = Symbol('123');
+
+const anotherId = Symbol('123');
+console.log(id);
+console.log(anotherId);
+console.log(id == anotherId);
+
+const bigNumber = 7834727382923234342334n
+console.log(bigNumber)
+
+
+// Non premitive/ refernce type : Array, Objects, functions
+
+const heros = ["shaktiman", "nagraj", "doga"];
+
+let myObj = {
+    name: "Manish",
+    age: 23,
+    class: "AL-ML",
+    div: 'A'
+}
+
+function printhello(){
+    console.log("Hello");
+}
+printhello()
+
+
+// arrow function
+let myfunction = ()=>{
+    console.log("Hello world");
+}
+myfunction()
+
+
+
+console.log(typeof num);
+console.log(typeof heros);
+console.log(typeof outsidetemp);
+console.log(typeof myfunction);
