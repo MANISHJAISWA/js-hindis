6147246e16f301ffed00aa68fb5e2fc4365bbe75
MANISH JAISWAL <120405796+MANISHJAISWA@users.noreply.github.com>
2024-04-25 18:31:30 +0000

    array1 completed

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -1,4 +1,70 @@
 // arrays
 
-const myArr = [0, 1, 2, 3, 4, 5]
-console.log(myArr);
+// const myArr = [0, 1, 2, 3, 4, 5]
+// console.log(myArr);
+// console.log(myArr[4]);
+
+// const heros = ["shaktiman", "nagraj"]
+// console.log(heros);
+
+// const arr2 = new Array(4, 5, 6, 7, 8)
+// console.log(arr2);
+// console.log(arr2[4]);
+// console.log(typeof arr2);   // array - object
+
+
+// Array methods
+
+// 1. push and pop
+
+// const arr3  = [3, 4, 5, 6, 8 , 9]
+// arr3.push(74);
+// arr3.push(89);
+// console.log(arr3);
+
+// arr3.pop()
+// console.log(arr3);
+
+
+// 2. shift and unshift
+
+// const arr3  = [3, 4, 5, 6, 8 , 9]
+// arr3.unshift(14)   // insert at starting of array
+// console.log(arr3);
+
+// arr3.shift()
+// console.log(arr3);
+
+
+// includes
+// const arr4  = [13, 14, 15, 16, 18 , 19]
+// console.log(arr4.includes(14));
+// console.log(arr4.includes(873));
+
+//indexof
+// const arr4  = [13, 14, 15, 16, 18 , 19]
+// console.log(arr4.indexOf(16));
+// console.log(arr4.indexOf(727));    // -1 means not present
+
+//join
+// const arr4  = [13, 14, 15, 16, 18 , 19]
+// const newArr = arr4.join()  // convert array in to string
+// console.log(arr4);          
+// console.log(newArr);
+// console.log(typeof newArr);
+
+
+// slice, splice
+// const myArr = [78, 22, 34, 56, 78]
+// console.log("A  ",myArr);
+
+// const myn1 = myArr.slice(0,2)     //doesnot manipulate original array
+// console.log("B", myn1);
+// console.log(myArr)
+
+// const myn2 = myArr.splice(3,5)   // manipulate original array
+// console.log("C",myn2);
+// console.log(myArr)
+
+ 
+
