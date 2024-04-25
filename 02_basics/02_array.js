4823a86ed5b7afb403c73fd99a767fd5c0e2ea12
MANISH JAISWAL <120405796+MANISHJAISWA@users.noreply.github.com>
2024-04-25 19:09:58 +0000

    array

---
+++ 02_basics/02_array.js
@@ -0,0 +1,13 @@
+const marvelHeros = ["Thor", "IronMan", "SpiderMan"]
+const dcHeros = ["superMan", "flash", "batman"]
+
+// marvelHeros.push(dcHeros)
+// console.log(marvelHeros);
+
+// console.log(marvelHeros);
+// console.log(marvelHeros[3]);
+// console.log(marvelHeros[3][2]);
+
+marvelHeros.concat(dcHeros)
+console.log(marvelHeros);
+
