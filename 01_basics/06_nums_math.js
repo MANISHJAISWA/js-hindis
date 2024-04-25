31e1d61d0dbb79353a279f374ef96b392ab58960
MANISH JAISWAL <120405796+MANISHJAISWA@users.noreply.github.com>
2024-04-25 12:08:43 +0000

    updTed

--- 01_basics/06_nums_math.js
+++ 01_basics/06_nums_math.js
@@ -14,5 +14,6 @@ const otherNumber = 23.4383432
 // console.log(otherNumber.toPrecision(4))
 
 const hundreds = 100000000000000
-console.log(hundreds.toLocaleString())
-console.log(hundreds.toExponential())
+// console.log(hundreds.toLocaleString())
+// console.log(hundreds.toExponential())
+
