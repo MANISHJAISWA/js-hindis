12c95216f8d3aa5214656fd03f4d3b55221d4af9
MANISH JAISWAL <120405796+MANISHJAISWA@users.noreply.github.com>
2024-04-25 20:20:52 +0000

    completed

--- 02_basics/02_array.js
+++ 02_basics/02_array.js
@@ -8,6 +8,24 @@ const dcHeros = ["superMan", "flash", "batman"]
 // console.log(marvelHeros[3]);
 // console.log(marvelHeros[3][2]);
 
-marvelHeros.concat(dcHeros)
-console.log(marvelHeros);
+// const allHeros = marvelHeros.concat(dcHeros)
+// console.log(allHeros);
+
+// const allNewHeros = [...marvelHeros, ...dcHeros]     //spread method
+// console.log(allNewHeros);
+
+
+// const anotherArray = [1, 2, 3,[4, 5, 6], 7, [6, 7, [4, 5]]]
+// const realAnotherArray = anotherArray.flat(Infinity)
+// console.log(realAnotherArray);
+
+// console.log(Array.isArray("Manish"));
+// console.log(Array.from("Manish"));
+// console.log(Array.from({name:"Manish"}));           //interesting
+
+let score1 = 100
+let score2 = 200
+let score3 = 300
+console.log(Array.of(score1, score2, score3));
+
 
