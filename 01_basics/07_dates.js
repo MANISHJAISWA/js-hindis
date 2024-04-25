db6e87c555cea393f4719bf0f5fd65d3e8dcea6e
MANISH JAISWAL <120405796+MANISHJAISWA@users.noreply.github.com>
2024-04-25 17:26:24 +0000

    basic1 completed

--- 01_basics/07_dates.js
+++ 01_basics/07_dates.js
@@ -10,7 +10,7 @@ const myDate = new Date()
 // console.log(myDate.toLocaleTimeString());         //1:25:09 PM
 
 
-console.log(typeof myDate);
+// console.log(typeof myDate);
 
 // const myNewDate = new Date(2024, 3, 25);
 // console.log(myNewDate.toDateString());
@@ -22,10 +22,28 @@ const myCreatedDate = new Date(2024,3,25,7,7)
 // console.log(myCreatDate.toLocaleString())  //mm/dd/yy
 
 
-// const myCreatDate = new Date("03-25-2024")
-// console.log(myCreatDate.toLocaleString())  //mm/dd/yy
+const myCreateDate = new Date("03-25-2024")
+// console.log(myCreateDate.toLocaleString())  //mm/dd/yy
 
 
 // timeStamps
 
-let myTimeStamp = 
+let myTimeStamp = Date.now()
+// console.log(myTimeStamp);
+// console.log(myCreateDate);
+// console.log(myCreateDate.getTime());
+
+// console.log(Math.floor(Date.now()/1000));    // seconds
+
+let newDate = new Date()
+console.log(newDate);
+console.log(newDate.getMonth()+1);
+console.log(newDate.getFullYear());
+console.log(newDate.getDay());
+console.log(newDate.getTime());
+
+newDate.toLocaleString('default',{
+    weekday:"long",
+    timeZone:'Coordinated'
+
+})
