c9a6804b77a49c9d0e9b54eff6d8c5c2fa2d2ba6
MANISH JAISWAL <120405796+MANISHJAISWA@users.noreply.github.com>
2024-04-25 13:02:58 +0000

    math and num

--- 01_basics/06_nums_math.js
+++ 01_basics/06_nums_math.js
@@ -17,3 +17,34 @@ const hundreds = 100000000000000
 // console.log(hundreds.toLocaleString())
 // console.log(hundreds.toExponential())
 
+//**********************************MATH********************* */
+// console.log(Math);
+// console.log(Math.E);
+// console.log(Math.LN2);
+// console.log(Math.LN10);
+// console.log(Math.LOG2E);
+// console.log(Math.LOG10E);
+// console.log(Math.PI);
+// console.log(Math.SQRT1_2);
+// console.log(Math.abs(-8));
+// console.log(Math.abs(9.635-5.837));
+// console.log(Math.pow(4,2));
+// console.log(Math.min(6, 2));
+// console.log(Math.max(12,133));
+// console.log(Math.sqrt(169));
+// console.log(Math.sin(0.523599)); // 30deg
+// console.log(Math.ceil(4.0112));  // 5
+// console.log(Math.floor(4.22));   // 4
+// console.log(Math.exp(1));
+// console.log(Math.log(10));
+// console.log(Math.round(5.7777));
+
+// console.log(Math.random());   // 0-1
+// console.log(Math.floor((Math.random()*10)+1));          //1-10
+// console.log(Math.floor((Math.random()* 100)+1));       // 1-100
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
+
