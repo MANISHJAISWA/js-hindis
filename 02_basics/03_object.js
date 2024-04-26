422a8b4f260e7345dbde8ce0f2c8ba8490d2c6a5
MANISH JAISWAL <120405796+MANISHJAISWA@users.noreply.github.com>
2024-04-26 18:30:50 +0000

    completed

--- 02_basics/03_object.js
+++ 02_basics/03_object.js
@@ -2,17 +2,51 @@
 // object.create
 
 //object literals
+
+const mySym = Symbol("key1")
+
+
 const jsUser = {
     name:"Manish",
-    "full  Name":"Manish Jaiswal",
+    [mySym]: "key1",                       // symbol
+    "full Name":"Manish Jaiswal",    
     age:18,
     email:"manish@email.com",
     isLoggedIn:true,
     lastLoginDays:["Monday","wednesday","saturday"]
 }
-console.log(jsUser.email);
-console.log(jsUser["email"])
+// console.log(jsUser.email);
+// console.log(jsUser["email"])  // another method to access
 
 // console.log(jsUser.full Name);   // show error
-console.log(jsUser["fullName"]);
+// console.log(jsUser["full Name"]);
+// console.log(typeof jsUser["full Name"]);
+
+// console.log(jsUser[mySym]);
+// console.log(typeof jsUser[mySym]);
+
+
+// jsUser.email = "manisharyan@gmail.com"
+// console.log(jsUser);
+
+// Object.freeze(jsUser)
+
+// jsUser.name = "raj"           // doesnot change because of freeze
+// console.log(jsUser);     
+
+// add function in object
+
+
+jsUser.greeting = function(){
+    console.log("Hello Js User");
+}
+jsUser.greetingTwo = function(){
+    console.log(` Hello Js User ${this.name}`);
+}
+console.log(jsUser.greeting());
+console.log(jsUser.greetingTwo());
+
+
+
+
 
